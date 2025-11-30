51deaac1d10c6c131b7be6b1b044c4286970e632
matemali <mateusz.malinowski.1991@gmail.com>
2025-11-30 15:22:40 +0100

    class definitions

---
+++ FractionalClass.py
@@ -0,0 +1,22 @@
+from abc import dataclass, abstractmethod, classmethod
+
+@dataclass(order=True)
+class Fractional():
+    
+    x: int
+    y: int
+
+    def __init__(self, x:int, y:int):
+        self.x = x
+        self.y = y
+    
+    def __str__(self):
+        return f"{self.x}/{self.y}"
+    
+    def __repr__(self):
+        return f"Fractional({self.x}, {self.y})"
+
+
+x1 = Fractional(69, 420)
+print(x1)
+print(x1.__repr__())
