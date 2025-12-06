aac64833fd885e773d6bbaaa7de46521f34069bf
matemali <mateusz.malinowski.1991@gmail.com>
2025-12-06 15:57:14 +0100

    additional under classes

--- FractionalClass.py
+++ FractionalClass.py
@@ -3,7 +3,7 @@ class Fractional():
     x: int
     y: int
 
-    def __init__(self, x:int, y:int = 1 ):
+    def __init__(self, x:int, y:int = 1):
         self.x = x
         if y == 0:
             raise ValueError("Y value of fractional cannoot be zero!")
@@ -19,7 +19,7 @@ class Fractional():
     def __add__(self, fract):
         x_n = self.x * fract.y + self.y * fract.x
         y_n = self.y * fract.y
-        return Fractional(x_n, y_n)
+        return Fractional(x1.to_decimal()_n, y_n)
     
     def __mul__(self, fract):
         x_n = self.x * fract.x
@@ -40,10 +40,22 @@ class Fractional():
         return self.x / self.y
     
     def __lt__(self, fract):
+        return self.to_decimal() < fract.to_decimal()
+    
+    def __gt__(self, fract):
+        return self.to_decimal() > fract.to_decimal()
+    
+    def __le__(self, fract):
+        return self.to_decimal() <= fract.to_decimal()
+    
+    def __ge__(self, fract):
+        return self.to_decimal() >= fract.to_decimal()
+    
+    def __eq__(self, fract):
         return self.to_decimal() == fract.to_decimal()
 
-
 x1 = Fractional(1, 4)
+x1 = 1
 x2 = Fractional(2, 3)
 
 x3 = x1 + x2
@@ -63,7 +75,19 @@ print(x6)
 print(x6.to_decimal())
 
 x7 = Fractional(2,8)
+print(f"{x1 = }")
+print(f"{x1.to_decimal()}")
+
+print(f"{x7 = }")
+print(f"{x7.to_decimal()}")
+
+print({x7.to_decimal()}, {x1.to_decimal()})
+
 print(x1 == x7)
+print(x1 > x2)
+print(x1 < x2)
+print(x1 >= x7)
+print(x1 <= x7) 
 
 #x7 = Fractional(20, 0)
 
